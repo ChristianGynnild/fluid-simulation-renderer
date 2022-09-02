30cea021d65be6ae3e4b05347b2c8822e00d57cc
ChristianGynnild <>
2022-09-02 10:44:53 +0200

    Added arrays to hold simulation data

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use image::RgbImage;
 
 
-const width:u32 = 32;
-const height:u32 = 32;
+const width:usize = 32;
+const height:usize = 32;
 
 fn save_image(vector:Vec<u8>){
-    RgbImage::from_raw(width, height, vector);
+    RgbImage::from_raw(width as u32, height as u32, vector);
 }
 
 fn main(){    
+    let density  = [[[0];height];width];
+    let density0 = [[[0];height];width];
 
+    let velocity  = [[[0,0];height];width];
+    let velocity0 = [[[0,0];height];width];
 }
