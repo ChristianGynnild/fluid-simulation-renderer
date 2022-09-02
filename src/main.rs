ded62ec23bf8269a1371e05f8517ec79078c36a0
ChristianGynnild <>
2022-09-02 10:35:59 +0200

    Added function to save a vector as a image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use image::RgbImage;
+
+
+const width:u32 = 32;
+const height:u32 = 32;
+
+fn save_image(vector:Vec<u8>){
+    RgbImage::from_raw(width, height, vector);
+}
+
+fn main(){    
+
 }
